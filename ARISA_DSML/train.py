bb3420c441d576d3219c46b105e5cae2b29caec1
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2025-05-08 19:44:32 +0000

    test commit

--- ARISA_DSML/train.py
+++ ARISA_DSML/train.py
@@ -21,6 +21,7 @@ from ARISA_DSML.config import (
 from ARISA_DSML.helpers import get_git_commit_hash
 from mlflow.models.signature import infer_signature
 
+#test commit 
 
 import os
 os.environ["MLFLOW_MODEL_SIGNATURE_INFERENCE_TIMEOUT"] = "60"
